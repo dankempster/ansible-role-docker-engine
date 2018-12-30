afc96dbec6c40d63243ac5792a49401cc70c78ad
Dan Kempster <me@dankempster.co.uk>
2018-12-30 14:39:24 +0000

    Fix test.sh script

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-molecule -e molecule/centos7_env.yml test && \
-molecule -e molecule/debian9_env.yml test
+molecule -e molecule/centos7_env.yml test \
+&& molecule -e molecule/debian9_env.yml test
